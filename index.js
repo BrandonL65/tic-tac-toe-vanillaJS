f2eab8d0252d87a138221ef2d8ea1283b9f543de
Brandon Lau <brandon.lau86@gmail.com>
2020-08-31 10:53:00 -0400

    Add tiles and ability to mark tiles

--- index.js
+++ index.js
@@ -8,15 +8,47 @@ let allTiles = document.querySelectorAll(".tile");
 class App {
   constructor() {
     this.turn = "X";
+    this.state = new Array(9).fill("");
     this.run();
   }
 
   run() {
     this.changeTurnText();
+    this.addClicksToTiles();
   }
   changeTurnText() {
-    let currentText = turnText.innerHTML;
-    turnText.innerHTML = `${currentText} ${this.turn}`;
+    let currentText = `Turn: ${this.turn}`;
+    turnText.innerHTML = currentText;
+  }
+  addClicksToTiles() {
+    allTiles.forEach((tile) => {
+      tile.addEventListener("click", () => {
+        console.log(tile.classList);
+        this.updateTileText(tile);
+        this.updateState(tile);
+        this.changeTurn();
+        this.changeTurnText();
+        console.log(this.state);
+      });
+    });
+  }
+  updateTileText(tile) {
+    tile.innerHTML = "";
+    let letter = document.createElement("p");
+    letter.classList.add("tile-inner-symbol");
+    letter.innerHTML = this.turn;
+    tile.appendChild(letter);
+  }
+  updateState(tile) {
+    let tileNumber = tile.classList[1].split("-")[1];
+    this.state[`${tileNumber - 1}`] = this.turn;
+  }
+  changeTurn() {
+    if (this.turn === "X") {
+      this.turn = "O";
+    } else {
+      this.turn = "X";
+    }
   }
 }
 
